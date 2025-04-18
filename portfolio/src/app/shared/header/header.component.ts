74a6bad11264de4951a71a16329f7ae4760d1bf7
Stefan Redl <bernhardlaus@gmx.at>
2025-04-18 11:22:43 +0200

    started to design the first page

--- portfolio/src/app/shared/header/header.component.ts
+++ portfolio/src/app/shared/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
+import { TitleComponent } from "../../title/title.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, TitleComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
