b88f6558153a904e5e8ab8a8ea213c36fb95d763
Stefan Redl <bernhardlaus@gmx.at>
2025-04-22 16:46:40 +0200

    started to design the about me section

--- portfolio/src/app/about-me/about-me.component.ts
+++ portfolio/src/app/about-me/about-me.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
