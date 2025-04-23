c621699a19dd4be325ee0be5da576d3a45331bdc
Stefan Redl <bernhardlaus@gmx.at>
2025-04-23 20:50:30 +0200

    finished skill section

--- portfolio/src/app/skills/skills.component.ts
+++ portfolio/src/app/skills/skills.component.ts
@@ -1,12 +1,86 @@
 import { Component } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [ MatTooltipModule ],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
+  skills = [
+    {
+      name: 'Angular', 
+      image: '/assets/skills/angular.png'
+    },
+    {
+      name: 'TypeScript', 
+      image: '/assets/skills/typescript.png'
+    },
+    {
+      name: 'JavaScript', 
+      image: '/assets/skills/javascript.png'
+    },
+    {
+      name: 'HTML', 
+      image: '/assets/skills/html.png'
+    },
+    {
+      name: 'Scrum', 
+      image: '/assets/skills/scrum.png'
+    },
+    {
+      name: 'Firebase', 
+      image: '/assets/skills/firebase.png'
+    },
+    {
+      name: 'Git', 
+      image: '/assets/skills/git.png'
+    },
+    {
+      name: 'CSS', 
+      image: '/assets/skills/css.png'
+    },
+    {
+      name: 'Rest Api', 
+      image: '/assets/skills/api.png'
+    },
+    {
+      name: 'Material Design', 
+      image: '/assets/skills/material des..png'
+    },
+    {
+      name: 'Python',
+      image: '/assets/skills/python.png'
+    },
+    {
+      name: 'Linux',
+      image: '/assets/skills/linux.png'
+    },
+    {
+      name: 'Docker',
+      image: '/assets/skills/docker.png'
+    },
+    {
+      name: 'SQL',
+      image: '/assets/skills/sql.png'
+    },
+    {
+      name: 'Growth Mindset', 
+      image: '/assets/skills/grow.png'
+    },
+  ]
 
+  wantToLearn = [
+    {
+      name: 'React',
+      image: '/assets/skills/react.png'
+    },
+    {
+      name: 'Vue JS',
+      image: '/assets/skills/vue js.png'
+    },
+
+  ]
 }
