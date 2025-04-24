218ac657b990b245cb963fb1aa979dbaab6e9b8e
Stefan Redl <bernhardlaus@gmx.at>
2025-04-24 21:45:58 +0200

    finish my-work section

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -5,11 +5,22 @@ import { HeaderComponent } from "./shared/header/header.component";
 import { HeroComponent } from "./hero/hero.component";
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { SkillsComponent } from "./skills/skills.component";
+import { MyWorkComponent } from "./my-work/my-work.component";
+import { ProjectsComponent } from "./projects/projects.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, HeroComponent, AboutMeComponent, SkillsComponent],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    HeaderComponent, 
+    HeroComponent, 
+    AboutMeComponent, 
+    SkillsComponent, 
+    MyWorkComponent,
+    ProjectsComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
