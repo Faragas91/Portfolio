69d15cde2ead8349ec98f72692bb43432554c9ce
Stefan Redl <stefan.redl@isis-papyrus.com>
2025-04-30 10:37:26 +0200

    finished footer translation

--- portfolio/src/app/shared/footer/footer.component.ts
+++ portfolio/src/app/shared/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LanguageService } from '../../service/language.service';
 
 @Component({
   selector: 'app-footer',
@@ -8,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  constructor(public languageService: LanguageService){}
 
+  legalNotice: string = '';
+
+  ngOnInit(){
+    this.languageService.language$.subscribe(lang => {
+      this.legalNotice = this.languageService.getTranslation('legalNotice');
+    }) 
+  }
 }
