ba768d8a05deacbcf0166b78d9dc0978aad0a971
Stefan Redl <bernhardlaus@gmx.at>
2025-05-12 18:57:15 +0200

    set right paths

--- portfolio/src/app/skills/skills.component.ts
+++ portfolio/src/app/skills/skills.component.ts
@@ -19,74 +19,74 @@ export class SkillsComponent {
   skills = [
     {
       name: 'Angular', 
-      image: './assets/skills/angular.png'
+      image: 'assets/skills/angular.png'
     },
     {
       name: 'TypeScript', 
-      image: './assets/skills/typescript.png'
+      image: 'assets/skills/typescript.png'
     },
     {
       name: 'JavaScript', 
-      image: './assets/skills/javascript.png'
+      image: 'assets/skills/javascript.png'
     },
     {
       name: 'HTML', 
-      image: './assets/skills/html.png'
+      image: 'assets/skills/html.png'
     },
     {
       name: 'Scrum', 
-      image: './assets/skills/scrum.png'
+      image: 'assets/skills/scrum.png'
     },
     {
       name: 'Firebase', 
-      image: './assets/skills/firebase.png'
+      image: 'assets/skills/firebase.png'
     },
     {
       name: 'Git', 
-      image: './assets/skills/git.png'
+      image: 'assets/skills/git.png'
     },
     {
       name: 'CSS', 
-      image: './assets/skills/css.png'
+      image: 'assets/skills/css.png'
     },
     {
       name: 'Rest Api', 
-      image: './assets/skills/api.png'
+      image: 'assets/skills/api.png'
     },
     {
       name: 'Material Design', 
-      image: './assets/skills/material des..png'
+      image: 'assets/skills/material des..png'
     },
     {
       name: 'Python',
-      image: './assets/skills/python.png'
+      image: 'assets/skills/python.png'
     },
     {
       name: 'Linux',
-      image: './assets/skills/linux.png'
+      image: 'assets/skills/linux.png'
     },
     {
       name: 'Docker',
-      image: './assets/skills/docker.png'
+      image: 'assets/skills/docker.png'
     },
     {
       name: 'SQL',
-      image: './assets/skills/sql.png'
+      image: 'assets/skills/sql.png'
     },
     {
       name: 'Growth Mindset', 
-      image: './assets/skills/grow.png'
+      image: 'assets/skills/grow.png'
     },
   ]
 
   wantToLearn = [
     {
       name: 'React',
-      image: './assets/skills/react.png'
+      image: 'assets/skills/react.png'
     },
     {
       name: 'Vue JS',
-      image: './assets/skills/vue js.png'
+      image: 'assets/skills/vue js.png'
     },
 
   ]
